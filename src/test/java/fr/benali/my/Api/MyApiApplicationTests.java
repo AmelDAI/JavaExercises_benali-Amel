8e4de5ae556991a2756d66011215fd540552ea71
Amel <amelbenali.math@gmail.com>
2023-06-21 15:03:29 +0300

    Spring Boot project initial

---
+++ src/test/java/fr/benali/my/Api/MyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.benali.my.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
